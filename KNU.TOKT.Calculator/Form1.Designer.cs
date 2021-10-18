c9e238d1426a6a87d633f4cb4104904b5355e094
Oleksii Bohusevych <olboh@smart-it.com>
2021-10-18 11:01:57 +0300

    added data grid, not tested

--- KNU.TOKT.Calculator/Form1.Designer.cs
+++ KNU.TOKT.Calculator/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace KNU.TOKT.Calculator
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,48 +29,147 @@ namespace KNU.TOKT.Calculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCalculate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxExpression = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxArgument = new System.Windows.Forms.TextBox();
+            this.dataGridTabulation = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonVisualize = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTabulation)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonCalculate
             // 
-            this.button1.Location = new System.Drawing.Point(469, 91);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Calculate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonCalculate.Location = new System.Drawing.Point(851, 91);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(112, 34);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(621, 93);
+            this.label1.Location = new System.Drawing.Point(1003, 93);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.Size = new System.Drawing.Size(0, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = string.Empty;
             // 
-            // textBox1
+            // textBoxExpression
             // 
-            this.textBox1.Location = new System.Drawing.Point(80, 91);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(332, 31);
-            this.textBox1.TabIndex = 2;
+            this.textBoxExpression.Location = new System.Drawing.Point(149, 91);
+            this.textBoxExpression.Name = "textBoxExpression";
+            this.textBoxExpression.Size = new System.Drawing.Size(654, 31);
+            this.textBoxExpression.TabIndex = 2;
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "x = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "f (x) = ";
+            // 
+            // textBoxArgument
+            // 
+            this.textBoxArgument.Location = new System.Drawing.Point(149, 46);
+            this.textBoxArgument.Name = "textBoxArgument";
+            this.textBoxArgument.Size = new System.Drawing.Size(150, 31);
+            this.textBoxArgument.TabIndex = 5;
+            // 
+            // dataGridTabulation
+            // 
+            this.dataGridTabulation.AllowUserToAddRows = false;
+            this.dataGridTabulation.AllowUserToDeleteRows = false;
+            this.dataGridTabulation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTabulation.Location = new System.Drawing.Point(338, 216);
+            this.dataGridTabulation.Name = "dataGridTabulation";
+            this.dataGridTabulation.ReadOnly = true;
+            this.dataGridTabulation.RowHeadersWidth = 62;
+            this.dataGridTabulation.RowTemplate.Height = 33;
+            this.dataGridTabulation.Size = new System.Drawing.Size(378, 391);
+            this.dataGridTabulation.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "x1 = ";
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Location = new System.Drawing.Point(149, 216);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(150, 31);
+            this.textBoxX1.TabIndex = 8;
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Location = new System.Drawing.Point(149, 275);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(150, 31);
+            this.textBoxX2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "x2 = ";
+            // 
+            // buttonVisualize
+            // 
+            this.buttonVisualize.Location = new System.Drawing.Point(163, 335);
+            this.buttonVisualize.Name = "buttonVisualize";
+            this.buttonVisualize.Size = new System.Drawing.Size(112, 34);
+            this.buttonVisualize.TabIndex = 12;
+            this.buttonVisualize.Text = "Visualize";
+            this.buttonVisualize.UseVisualStyleBackColor = true;
+            this.buttonVisualize.Click += new System.EventHandler(this.buttonVisualize_Click);
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1241, 658);
+            this.Controls.Add(this.buttonVisualize);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridTabulation);
+            this.Controls.Add(this.textBoxArgument);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxExpression);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Controls.Add(this.buttonCalculate);
+            this.Name = "FormMain";
             this.Text = "Calculator";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTabulation)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,9 +177,19 @@ namespace KNU.TOKT.Calculator
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCalculate;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxExpression;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxArgument;
+        private System.Windows.Forms.DataGridView dataGridTabulation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonVisualize;
     }
 }
 
--- KNU.TOKT.Calculator/Form1.cs
+++ KNU.TOKT.Calculator/Form1.cs
@@ -1,25 +1,104 @@
 ﻿using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace KNU.TOKT.Calculator
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        private const int tabulatedValuesNumber = 10;
+        private const int plotValuesNumber = 100;
+
+        public FormMain()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FormMain_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonCalculate_Click(object sender, EventArgs e)
         {
-            var expression = textBox1.Text.Replace(" ", string.Empty);
-            var result = Calculator.Evaluate(expression);
-            label1.Text = result.ToString();
+            if (textBoxArgument.Text.Equals(string.Empty))
+            {
+                textBoxArgument.Text = 0.ToString();
+            }
+
+            var expressionText = textBoxExpression.Text;
+            var x = textBoxArgument.Text;
+
+            if (!double.TryParse(x, out var doubleValue))
+            {
+                HandleInvalidInput(textBoxArgument, GetNumericErrorMessage("Argument"));
+            }
+
+            var expression = expressionText
+                .Replace(nameof(x), x)
+                .Replace(" ", string.Empty);
+
+            try
+            {
+                var result = Calculator.Evaluate(expression);
+                label1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                HandleInvalidInput(textBoxExpression, ex.Message);
+            }
+        }
+
+        private void buttonVisualize_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBoxX1.Text, out var x1))
+            {
+                HandleInvalidInput(textBoxX1, GetNumericErrorMessage("Lower boundary"));
+            }
+
+            if (!double.TryParse(textBoxX2.Text, out var x2))
+            {
+                HandleInvalidInput(textBoxX1, GetNumericErrorMessage("Upper boundary"));
+            }
+
+            if (x1 >= x2)
+            {
+                HandleInvalidInput(textBoxX1, "Insufficient boundaries.");
+            }
+
+            var table = new DataTable();
+            table.Columns.AddRange(new DataColumn[3]
+            {
+                new DataColumn("#", typeof(int)),
+                new DataColumn("x", typeof(double)),
+                new DataColumn("f(x)",typeof(double))
+            });
+
+            var step = (x2 - x1) / tabulatedValuesNumber;
+            var expressionText = textBoxExpression.Text;
+            var x = x1;
+
+            for (int i = 0; i < tabulatedValuesNumber; ++i)
+            {
+                var expression = expressionText
+                    .Replace(nameof(x), x.ToString())
+                    .Replace(" ", string.Empty);
+
+                table.Rows.Add(i, Math.Round(x, 5), Calculator.Evaluate(expression));
+                x += step;
+            }
+
+            dataGridTabulation.DataSource = table;
+        }
+
+        private void HandleInvalidInput(TextBox textBox, string reason)
+        {
+            MessageBox.Show($"Invalid input, please resubmit. Reason: {reason}");
+        }
+
+        private string GetNumericErrorMessage(string issuer)
+        {
+            return $"{issuer} should be numeric.";
         }
     }
 }
